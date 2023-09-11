0f972c4354245dbe7c29bd477a2dd7e1765bdc69
Fajrullah Sejati <fajrullah_9_7@yahoo.com>
2023-09-11 14:45:29 +0700

    integrate cucumber

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,15 +1,30 @@
 package com.example;
 
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = "com.example")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter messageConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper objectMapper = new ObjectMapper();
+		converter.setObjectMapper(objectMapper);
+		return converter;
 	}
 
 }
