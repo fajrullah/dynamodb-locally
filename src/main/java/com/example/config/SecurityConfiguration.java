914360eaf27bf561362abdc92d6130553dfc4344
Fajrullah Sejati <fajrullah_9_7@yahoo.com>
2023-09-12 22:13:25 +0700

    commented unused

--- src/main/java/com/example/config/SecurityConfiguration.java
+++ src/main/java/com/example/config/SecurityConfiguration.java
@@ -43,69 +43,69 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    public SecurityWebFilterChain accountAuthorization(ServerHttpSecurity http, @Qualifier("opaWebClient")WebClient opaWebClient) {
-        return http
-                .httpBasic(withDefaults())
-                .authorizeExchange(exchanges -> {
-                    exchanges
-                            .pathMatchers("/api/**")
-                            .access(opaAuthManager(opaWebClient));
-                })
-                .build();
+//    public SecurityWebFilterChain accountAuthorization(ServerHttpSecurity http, @Qualifier("opaWebClient")WebClient opaWebClient) {
+//        return http
+//                .httpBasic(withDefaults())
+//                .authorizeExchange(exchanges -> {
+//                    exchanges
+//                            .pathMatchers("/api/**")
+//                            .access(opaAuthManager(opaWebClient));
+//                })
+//                .build();
+//
+//    }
+//
+//    public ReactiveAuthorizationManager<AuthorizationContext> opaAuthManager(WebClient opaWebClient) {
+//
+//        return (auth, context) -> {
+//            return opaWebClient.post()
+//                    .accept(MediaType.APPLICATION_JSON)
+//                    .contentType(MediaType.APPLICATION_JSON)
+//                    .body(toAuthorizationPayload(auth,context), Map.class)
+//                    .exchangeToMono(this::toDecision);
+//        };
+//    }
+//
+//    private Mono<AuthorizationDecision> toDecision(ClientResponse response) {
+//
+//        if ( !response.statusCode().is2xxSuccessful()) {
+//            return Mono.just(new AuthorizationDecision(false));
+//        }
+//
+//        return response
+//                .bodyToMono(ObjectNode.class)
+//                .map(node -> {
+//                    boolean authorized = node.path("result").path("authorized").asBoolean(false);
+//                    return new AuthorizationDecision(authorized);
+//                });
+//
+//    }
 
-    }
-
-    public ReactiveAuthorizationManager<AuthorizationContext> opaAuthManager(WebClient opaWebClient) {
-
-        return (auth, context) -> {
-            return opaWebClient.post()
-                    .accept(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(toAuthorizationPayload(auth,context), Map.class)
-                    .exchangeToMono(this::toDecision);
-        };
-    }
-
-    private Mono<AuthorizationDecision> toDecision(ClientResponse response) {
-
-        if ( !response.statusCode().is2xxSuccessful()) {
-            return Mono.just(new AuthorizationDecision(false));
-        }
-
-        return response
-                .bodyToMono(ObjectNode.class)
-                .map(node -> {
-                    boolean authorized = node.path("result").path("authorized").asBoolean(false);
-                    return new AuthorizationDecision(authorized);
-                });
-
-    }
-
-    private Publisher<Map<String,Object>> toAuthorizationPayload(Mono<Authentication> auth, AuthorizationContext context) {
-        return auth
-                .defaultIfEmpty(new AnonymousAuthenticationToken("**ANONYMOUS**", new Object(), Arrays.asList(new SimpleGrantedAuthority("ANONYMOUS"))))
-                .map( a -> {
-
-                    Map<String,String> headers = context.getExchange().getRequest()
-                            .getHeaders()
-                            .toSingleValueMap();
-
-                    Map<String,Object> attributes = ImmutableMap.<String,Object>builder()
-                            .put("principal",a.getName())
-                            .put("authorities",
-                                    a.getAuthorities()
-                                            .stream()
-                                            .map(g -> g.getAuthority())
-                                            .collect(Collectors.toList()))
-                            .put("uri", context.getExchange().getRequest().getURI().getPath())
-                            .put("headers",headers)
-                            .build();
-
-                    Map<String,Object> input = ImmutableMap.<String,Object>builder()
-                            .put("input",attributes)
-                            .build();
-
-                    return input;
-                });
-    }
+//    private Publisher<Map<String,Object>> toAuthorizationPayload(Mono<Authentication> auth, AuthorizationContext context) {
+//        return auth
+//                .defaultIfEmpty(new AnonymousAuthenticationToken("**ANONYMOUS**", new Object(), Arrays.asList(new SimpleGrantedAuthority("ANONYMOUS"))))
+//                .map( a -> {
+//
+//                    Map<String,String> headers = context.getExchange().getRequest()
+//                            .getHeaders()
+//                            .toSingleValueMap();
+//
+//                    Map<String,Object> attributes = ImmutableMap.<String,Object>builder()
+//                            .put("principal",a.getName())
+//                            .put("authorities",
+//                                    a.getAuthorities()
+//                                            .stream()
+//                                            .map(g -> g.getAuthority())
+//                                            .collect(Collectors.toList()))
+//                            .put("uri", context.getExchange().getRequest().getURI().getPath())
+//                            .put("headers",headers)
+//                            .build();
+//
+//                    Map<String,Object> input = ImmutableMap.<String,Object>builder()
+//                            .put("input",attributes)
+//                            .build();
+//
+//                    return input;
+//                });
+//    }
 }
