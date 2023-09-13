c0a5072f45906dfb11c62f98c6eb48e65a5ac845
Fajrullah Sejati <fajrullah_9_7@yahoo.com>
2023-09-13 10:51:19 +0700

    last works

--- src/main/java/com/example/config/OpaProperties.java
+++ src/main/java/com/example/config/OpaProperties.java
@@ -3,12 +3,14 @@ package com.example.config;
 import lombok.Data;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Nonnull;
 
 @ConfigurationProperties(prefix = "opa")
 @Data
 @Getter
+@Configuration
 public class OpaProperties {
     @Nonnull
     private String endpoint = "http://localhost:8181";
