2cdf9eeba97fcf596ab12572c7c17ab5e6d26e17
Fajrullah Sejati <fajrullah_9_7@yahoo.com>
2023-09-20 14:40:58 +0700

    fixed the rego

---
+++ src/main/java/com/example/config/OPADataRequest.java
@@ -0,0 +1,16 @@
+package com.example.config;
+import java.util.Map;
+
+public class OPADataRequest {
+
+    Map<String, Object> input;
+
+    public OPADataRequest(Map<String, Object> input) {
+        this.input = input;
+    }
+
+    public Map<String, Object> getInput() {
+        return this.input;
+    }
+
+}
---
+++ src/main/java/com/example/config/OPADataResponse.java
@@ -0,0 +1,18 @@
+package com.example.config;
+public class OPADataResponse {
+
+    private boolean result;
+
+    public OPADataResponse() {
+
+    }
+
+    public boolean getResult() {
+        return this.result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+}
